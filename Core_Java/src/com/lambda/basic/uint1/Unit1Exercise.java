33bb2d4473915aa0685053641175589b11407d14
chamanshareef87 <chamanshareef@gmail.com>
2017-11-01 23:23:41 +0530

    adding lambda examples

---
+++ Core_Java/src/com/lambda/basic/uint1/Unit1Exercise.java
@@ -0,0 +1,24 @@
+package com.lambda.basic.uint1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit1Exercise {
+
+	public static void main(String[] args) {
+		List<Person> people = Arrays.asList(
+				new Person("Charle", "Dicken", 60),
+				new Person("Lewis", "Carroll", 25),
+				new Person("Thomas", "Carlyle", 25),
+				new Person("Charlotte", "Bronte", 25),
+				new Person("Matthew", "Arnold", 25)
+				);
+	}
+	
+	//Solr list by last name
+	
+	//Create method that prints all elements in the list
+	
+	// create method that prints all people that have last name begingin with C
+	
+}
